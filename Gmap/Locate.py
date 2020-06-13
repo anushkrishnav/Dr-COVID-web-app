297191f3f7ad47aebc063b1f5de32f3adb3edd6e
anushkrishnav <anush.venkatakrishna@gmail.com>
2020-06-13 10:14:45 +0530

    Combined Scripts

--- Gmap/Locate.py
+++ Gmap/Locate.py
@@ -1,8 +1,17 @@
 from googleplaces import GooglePlaces, types, lang 
-from Geo_Location_finder import *
-  
-
+import folium
+import requests
 API_KEY='AIzaSyBeQ8EMs01Cnt9IHZXwqzUQOVqwra-WPJk'
+def GetUserGeoLocation():
+    url = 'http://ipinfo.io/json '
+    payload = {}
+    files = {}
+    headers= {}
+    
+    response = requests.request("GET", url, headers=headers, data = payload, files = files)
+    data = response.json()
+    list1=data["loc"].split(sep=",")
+    return list1
 def GetHospitalLocation():  
     # Initialising the GooglePlaces constructor 
     google_places = GooglePlaces(API_KEY) 
@@ -25,5 +34,24 @@ def GetHospitalLocation():
     for place in query_result.places:
         latlng.append([str(place.name),float(place.geo_location['lat']),float(place.geo_location['lng'])]) 
     return latlng 
+# Map method of folium return Map object 
+Userloc=GetUserGeoLocation()
+hosname=[]
 
-GetHospitalLocation()
+# Here we pass coordinates of User  
+# and starting Zoom level = 11 
+my_map2 = folium.Map(location = Userloc, zoom_start = 30) 
+HospitalLoc=GetHospitalLocation()
+#we now obtain a list of hospital names and locations  
+for valu in HospitalLoc:
+    #Splitting the hospital name from location
+    hosname.append(valu.pop(0))
+# save method of Map object will create a map 
+# Creating multiple Parachute Marker with hospitalname popups   
+folium.Marker(location = Userloc,popup ="You", icon=folium.Icon(color='red',icon_color='white',icon='cloud')).add_to(my_map2) 
+for loc,Hsname in zip(HospitalLoc,hosname):
+    folium.Marker(location = loc,popup =Hsname,draggable=True).add_to(my_map2) 
+    
+    # save as html 
+
+my_map2.save("Hospitals.html")
